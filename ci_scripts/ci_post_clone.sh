5fb4fd2db524a78a3206258ccff4724d31d5e388
aguhernandez <aguhernandezbk@gmail.com>
2026-05-18 18:01:03 +0300

    Añadido script para instalar dependencias en la nube

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
+
+# 1. Instalar dependencias de Node
 npm install
+
+# 2. Instalar Capacitor CLI
 npm install @capacitor/cli
+
+# 3. Sincronizar (esto crea los archivos que Xcode busca)
 npx cap sync ios
+
+# 4. Forzar la resolución de paquetes de Swift
+xcodebuild -resolvePackageDependencies -project ios/App/App.xcodeproj -scheme App
